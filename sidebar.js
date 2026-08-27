eff99ccf943f1f9105ccb0f19639acc2302da3b8
stevehill2683-hue <stevehill2683@gmail.com>
2026-08-27 08:58:23 -0400

    Wire Israel sidebar links

--- sidebar.js
+++ sidebar.js
@@ -38,6 +38,40 @@ function buildSidebar() {
 
 } else if (category === "Israel") {
     subItem.classList.add("sidebar-item-israel");
+
+    const israelLinks = {
+        "General": "israel.html#title-1",
+        "Southern Stairs": "israel.html#title-2",
+        "Temple Mount": "israel.html#title-3",
+        "Masada": "israel.html#title-4",
+        "Sea Of Galilee": "israel.html#title-5",
+        "Temple Institute": "israel.html#title-6",
+        "Ramparts Walk Citadel": "israel.html#title-7",
+        "David & Goliath": "israel.html#title-11",
+        "Jordan Baptism": "israel.html#title-12",
+        "Garden Tomb": "israel.html#title-16",
+        "Model City": "israel.html#title-17",
+        "City of David": "israel.html#title-21",
+        "Bar Mitzvah": "israel.html#title-26",
+        "Rosh HaNikira Grottoes": "israel.html#title-28",
+        "Temple Mount Hezekiah": "israel.html#title-8",
+        "Megiddo": "israel.html#title-9",
+        "Golden Gate": "israel.html#title-10",
+        "Caesarea": "israel.html#title-13",
+        "Garden of Gethsemane": "israel.html#title-14",
+        "Mount of Beatitudes": "israel.html#title-18",
+        "Mount of Olives": "israel.html#title-22",
+        "Burnt House": "israel.html#title-23",
+        "Robinson's Arch": "israel.html#title-24",
+        "Tel‑Aviv": "israel.html#title-29",
+        "Misc Pictures": "israel.html#title-30",
+        "Nahariya Mission": "israel.html#title-25",
+        "Jerusalem Shopping": "israel.html#title-15",
+        "Western Wall & Rabbi's Tunnel": "israel.html#title-19",
+        "Capernaum": "israel.html#title-20"
+    };
+
+    subItem.href = israelLinks[sub] || "#";
 }
 
 sidebar.appendChild(subItem);
